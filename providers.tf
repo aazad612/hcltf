949a16ecfb0f590faadd62c2a453d0d6f865f510
JohneyA <aazads@aazads.us>
2021-12-25 15:18:32 -0500

    networking

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "k8s-aazads-us"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+provider "google" {
+  alias = "google-west"
+  #  credentials = file("../../../../zzz/credentials/k8s-aazads-us-gcp-service-infra.json")
+  project = "k8s-aazads-us"
+  region  = "us-west1"
+}
